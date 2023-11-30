036e9872c04d4c0a09e7f2694efe2e1b5a7381f2
HarryAhnHS <hongsuhahn@gmail.com>
2023-11-30 14:27:13 +0900

    Input validation for input field email JS responsive feedback

--- main.js
+++ main.js
@@ -13,7 +13,20 @@ function validateInput() {
                 nameLabel.classList.remove('valid-label');
             }
         }
-    
+        else if (input.id == "email") {
+            const emailLabel = document.querySelector("label[for=" + input.id + "]")
+            if (String(input.value).toLowerCase()
+            .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+                // console.log('valid email')
+                input.classList.add('valid-input');
+                emailLabel.classList.add('valid-label');
+            }
+            else {
+                input.classList.remove('valid-input');
+                emailLabel.classList.remove('valid-label');
+            }
+            
+        }
     });
 }
 
