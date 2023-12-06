b3fae2bd33d68cdc99b1680de2fb9f06df3bf8e5
HarryAhnHS <hongsuhahn@gmail.com>
2023-12-06 19:07:33 +0800

    Include theme and graphic template, update submit button functionality to reflect validity

--- main.js
+++ main.js
@@ -2,32 +2,88 @@ function validateInput() {
     const inputs = document.querySelectorAll(".sign-up-input");
     // Functions to validate each input
     inputs.forEach( function (input) {
-        if (input.id == "first_name" || input.id == "last_name") {
-            const nameLabel = document.querySelector("label[for=" + input.id + "]")
+        const label = document.querySelector("label[for=" + input.id + "]");
+        
+        if (input.id == "first_name" || input.id == "last_name") {  
             if (input.value != "") {
-                input.classList.add('valid-input');
-                nameLabel.classList.add('valid-label');
+                input.classList.add('validated');
             }
             else {
-                input.classList.remove('valid-input');
-                nameLabel.classList.remove('valid-label');
+                input.classList.remove('validated');
+            }
+        }
+        else if (input.id == "phone") {
+            if (String(input.value) != "" && String(input.value).match(/^[0-9]+$/)) {
+                input.classList.add('validated');
+            }
+            else {
+                input.classList.remove('validated');
             }
         }
         else if (input.id == "email") {
-            const emailLabel = document.querySelector("label[for=" + input.id + "]")
             if (String(input.value).toLowerCase()
             .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-                // console.log('valid email')
-                input.classList.add('valid-input');
-                emailLabel.classList.add('valid-label');
+                input.classList.add('validated');
             }
             else {
-                input.classList.remove('valid-input');
-                emailLabel.classList.remove('valid-label');
+                input.classList.remove('validated');
             }
-            
+        }
+        else if (input.id == 'pw') {
+            if (String(input.value).length >= 8)  {
+                input.classList.add('validated');
+            }
+            else {
+                input.classList.remove('validated');
+            }
+        }
+        else if (input.id == 'confirm_pw') {
+            if (String(signUpForm.pw.value) != "" && String(signUpForm.pw.value).length >= 8) {
+                if (String(input.value) == String(signUpForm.pw.value)) {
+                    input.classList.add('validated');
+                }
+                else {
+                    input.classList.remove('validated');
+                }
+            }
+            else {
+                input.classList.remove('validated');
+            }
         }
     });
 }
+
+// Validate all inputs to have class 'validated'
+
+
+const submit = document.querySelector('#submit-btn');
+submit.addEventListener('click', (e) => {
+    console.log('submit')
+    e.preventDefault();
+    const inputs = document.querySelectorAll(".sign-up-input");
+    let validAll = true;
+    inputs.forEach( function(input) {
+        const label = document.querySelector("label[for=" + input.id + "]");
+        if (!input.classList.contains('validated')) {
+            input.classList.add("invalid-input");
+            input.classList.remove("valid-input");
+            label.classList.add("invalid-label");
+            label.classList.remove("valid-label");
+            validAll = false;
+        }
+        else {
+            input.classList.remove("invalid-input");
+            input.classList.add("valid-input");
+            label.classList.remove("invalid-label");
+            label.classList.add("valid-label");
+        }
+    });
+
+    if (validAll) {
+        console.log('Valid form - submitting')
+        signUpForm.submit();
+    }
+
+})
 
 
